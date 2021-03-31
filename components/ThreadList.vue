b1ba385597a28fc88588812824e68b35f8d71bdf
jiangzhiji <1063010784@qq.com>
2021-04-01 07:44:33 +0800

    新增主页

--- components/ThreadList.vue
+++ components/ThreadList.vue
@@ -1,26 +1,26 @@
 <template>
-  <div class="mx-4"> 
-        <div class="h-40 bg-indigo-600  " v-for="thread in threadList" :key="thread.id">
-      <div class=" py-4 text-2xl" > {{thread.title}} </div>
-      <div class=" text-gray-300" v-if="thread.slug"> {{thread.slug}} </div>
-        <span>显示全部</span>
-        <div class="m-4">
-          <span class="m-2 border border-white border-2 border-solid">回答</span>
-          <span class="m-2 border border-white border-2 border-solid ml-2">关注</span>
-        </div>
-     </div>
+  <div>
+    <div class="px-4 bg-purple-800 bg-opacity-80" v-for="thread in threadList" :key="thread.id">
+      <div class="py-4 text-2xl text-gray-300 hover:text-red-700">{{thread.title}}</div>
+      <div class="inline text-gray-300" v-if="thread.slug">{{thread.slug}}</div>
+      <span class="hover:bg-red-200">显示全部</span>
+      <div class="mt-4">
+        <span class="px-2 py-1 border border-2 border-white border-solid">回答</span>
+        <span class="px-2 py-1 border border-2 border-white border-solid">关注</span>
+        <span class="px-2 py-1 text-gray-200">7 个回答</span>
+        <span class="px-2 py-1 text-gray-200">13 个关注</span>
+      </div>
+    </div>
   </div>
-     
 </template>
 
 <script>
-    export default {
-        props:{
-            threadList:Array
-        }
-    }
+export default {
+  props: {
+    threadList: Array
+  }
+};
 </script>
 
 <style scoped>
-
 </style>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>this is index</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
